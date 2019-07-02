298dcbe8c8ddbcdfe3eb5f9863091d7fa86e6d6e
gerardb83 <gbreitenbeck@gmail.com>
2019-07-01 22:36:05 -0400

    minor format fixes

--- src/Validations.java
+++ src/Validations.java
@@ -10,7 +10,6 @@ public class Validations {
 		boolean validEmail = validateEmail(scnr, "Please enter your email: ");
 		boolean validPhoneNumber = validatePhoneNumber(scnr, "Please enter your phone number (xxx-xxx-xxxx): ");
 		boolean validDate = validateDate(scnr, "Please enter your birthday (dd/mm/yyyy): ");
-
 	}
 
 	private static boolean validateName(Scanner scnr, String prompt) {
@@ -19,14 +18,12 @@ public class Validations {
 		do {
 			System.out.print(prompt);
 			input = scnr.nextLine();
-
 			if (input.matches("[A-Z][a-zA-Z]{0,30}")) {
 				isValid = true;
 				System.out.println("Valid entry");
 
 			} else {
 				System.out.println("Please enter a valid capitalized name");
-
 			}
 		} while (!isValid);
 		return isValid;
@@ -38,14 +35,11 @@ public class Validations {
 		do {
 			System.out.print(prompt);
 			input = scnr.nextLine();
-
 			if (input.matches("[a-zA-Z\\d]{5,30}@[a-zA-Z\\d]{5,10}.[a-zA-Z\\d]{2,3}")) {
 				isValid = true;
 				System.out.println("Valid entry");
-
 			} else {
 				System.out.println("Please enter a valid email");
-
 			}
 		} while (!isValid);
 		return isValid;
@@ -57,11 +51,9 @@ public class Validations {
 		do {
 			System.out.print(prompt);
 			input = scnr.nextLine();
-
 			if (input.matches("[\\d]{3}-[\\d]{3}-[\\d]{4}")) {
 				isValid = true;
 				System.out.println("Valid entry");
-
 			} else {
 				System.out.println("Please enter a valid phone number");
 
@@ -76,14 +68,11 @@ public class Validations {
 		do {
 			System.out.print(prompt);
 			input = scnr.nextLine();
-
 			if (input.matches("[\\d]{2}\\/[\\d]{2}\\/[\\d]{4}")) {
 				isValid = true;
 				System.out.println("Valid entry");
-
 			} else {
 				System.out.println("Please enter a valid date");
-
 			}
 		} while (!isValid);
 		return isValid;
