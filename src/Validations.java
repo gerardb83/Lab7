5a36224348872b3d5d528545efdb0f4038f65b68
gerardb83 <gbreitenbeck@gmail.com>
2019-07-02 08:47:02 -0400

    Now with HTML tag validator

--- src/Validations.java
+++ src/Validations.java
@@ -1,17 +1,17 @@
-
 import java.util.Scanner;
 
 public class Validations {
 
 	public static void main(String[] args) {
-
+		
 		Scanner scnr = new Scanner(System.in);
 		boolean validName = validateName(scnr, "Please enter your name: ");
 		boolean validEmail = validateEmail(scnr, "Please enter your email: ");
 		boolean validPhoneNumber = validatePhoneNumber(scnr, "Please enter your phone number (xxx-xxx-xxxx): ");
 		boolean validDate = validateDate(scnr, "Please enter your birthday (dd/mm/yyyy): ");
+		boolean validHtml = validateHtml(scnr, "Please enter an HTML element pair: "); 
 	}
-
+	
 	private static boolean validateName(Scanner scnr, String prompt) {
 		boolean isValid = false;
 		String input;
@@ -71,6 +71,22 @@ public class Validations {
 				System.out.println("Valid entry");
 			} else {
 				System.out.println("Please enter a valid date");
+			}
+		} while (!isValid);
+		return isValid;
+	}
+	
+	private static boolean validateHtml(Scanner scnr, String prompt) {
+		boolean isValid = false;
+		String input;
+		do {
+			System.out.print(prompt);
+			input = scnr.nextLine();
+			if (input.matches("\\<([a-zA-Z]{1}[\\w]*)\\>[\\s\\S]*\\<\\/\\1\\>")) {
+				isValid = true;
+				System.out.println("Valid entry");
+			} else {
+				System.out.println("Please enter a valid HTML");
 			}
 		} while (!isValid);
 		return isValid;
